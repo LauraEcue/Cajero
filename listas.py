1aeddea06ad8a249579a4a45105dbf5fa38f25ce
LauraEcue <lauravalentinacastillo23@gmail.com>
2026-05-12 20:01:28 -0500

    feat

--- listas.py
+++ listas.py
@@ -1,4 +1,7 @@
 frutas = ["Manzana", "Mango", "Pera", "Fresa", "Lulo", "Maracuya"]
 frutas.append("durazno")
+frutas[0] = "Laura castillo"
 for fruit in frutas:
     print(f"nombre: {fruit}")
+print("me gusta la " + frutas[5])
+
