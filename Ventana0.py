67f8d10f581c79a89ccc7b50d8433639aed9a912
LauraEcue <lauravalentinacastillo23@gmail.com>
2026-05-19 21:04:06 -0500

    feat

--- Ventana0.py
+++ Ventana0.py
@@ -1,4 +1,5 @@
 from tkinter import Tk, ttk
+from tkinter import messagebox
 
 if __name__ == "__main__":
     root = Tk()
@@ -8,10 +9,13 @@ if __name__ == "__main__":
     frm.grid()
     
     lbl = ttk.Button(frm, text="Ventana Python - ")
-    lbl.grid(column=0, row=0)
+    lbl.grid(column=1, row=0)
+
+    btn = ttk.Button(frm, text="información", command=root.destroy)
+    btn.grid(column=0, row=0)
     
     btn = ttk.Button(frm, text="Quit", command=root.destroy)
-    btn.grid(column=1, row=0)
+    btn.grid(column=2, row=0)
     
     root.mainloop()
     
